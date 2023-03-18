31fd4fe3c64a1bcbc27582df89c405ef0facc48f
scriptax <moradii97@outlook.com>
2023-03-18 23:51:08 +0330

    new UI + added some options + code refactoring

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cloudflare IP scanner
+Tests if cloudflare IPs are accessible for V2Ray purpose.
+[CF IP Scanner](https://scriptax.github.io/cfip/)
--- cis.js
+++ cis.js
@@ -1,323 +1,162 @@
-const cfIPv4 = {
-  London: [
-    '5.226.179.0/24',
-    // '5.226.181.0/24',
-    '8.12.10.0/24',
-    '12.221.133.0/24',
-    '23.141.168.0/24',
-    '23.178.112.0/24',
-    '23.247.163.0/24',
-    '31.43.179.0/24',
-    '38.67.242.0/24',
-    '45.8.104.0/22',
-    '45.8.211.0/24',
-    '45.12.30.0/23',
-    '45.14.174.0/24',
-    '45.80.111.0/24',
-    '45.84.59.0/24',
-    '45.85.118.0/23',
-    '45.87.175.0/24',
-    '45.94.169.0/24',
-    '45.95.241.0/24',
-    '45.131.4.0/22',
-    '45.131.208.0/22',
-    '45.133.247.0/24',
-    '45.137.99.0/24',
-    '45.142.120.0/24',
-    '45.145.28.0/23',
-    '45.158.56.0/24',
-    '45.159.216.0/22',
-    '64.21.2.0/24',
-    '65.205.150.0/24',
-    '66.81.247.0/24',
-    '66.81.255.0/24',
-    '72.52.113.0/24',
-    '80.94.83.0/24',
-    '89.47.56.0/23',
-    '89.116.250.0/24',
-    '89.207.18.0/24',
-    '91.192.107.0/24',
-    '91.193.58.0/23',
-    '91.195.110.0/24',
-    '91.199.81.0/24',
-    '91.221.116.0/24',
-    '93.114.64.0/23',
-    '95.214.178.0/23',
-    '103.11.212.0/24',
-    '103.11.214.0/24',
-    '103.79.228.0/23',
-    '103.112.176.0/24',
-    '103.121.59.0/24',
-    '103.156.22.0/23',
-    '103.160.204.0/24',
-    '103.168.172.0/24',
-    '103.169.142.0/24',
-    '103.172.111.0/24',
-    '154.84.175.0/24',
-    '103.204.13.0/24',
-    '103.244.116.0/22',
-    '154.85.9.0/24',
-    '104.234.158.0/24',
-    '104.254.140.0/24',
-    '108.165.216.0/24',
-    '154.85.99.0/24',
-    '123.253.174.0/24',
-    '141.11.194.0/23',
-    '141.193.213.0/24',
-    '146.19.22.0/24',
-    '147.78.121.0/24',
-    '147.78.140.0/24',
-    '147.185.161.0/24',
-    '154.51.129.0/24',
-    '154.51.160.0/24',
-    '154.83.2.0/24',
-    '154.83.22.0/24',
-    '154.83.30.0/24',
-    '154.84.14.0/23',
-    '154.84.16.0/24',
-    '154.84.20.0/23',
-    '154.84.24.0/24',
-    '154.84.26.0/23',
-    '154.219.3.0/24',
-    '156.237.4.0/23',
-    '156.238.14.0/24',
-    '156.238.18.0/23',
-    '156.239.152.0/23',
-    '156.239.154.0/24',
-    '159.112.235.0/24',
-    '159.246.55.0/24',
-    '160.153.0.0/24',
-    '162.44.104.0/22',
-    '168.100.6.0/24',
-    '170.114.45.0/24',
-    '170.114.46.0/24',
-    '170.114.52.0/24',
-    '172.83.72.0/23',
-    '172.83.76.0/24',
-    '185.207.92.0/24',
-    '174.136.134.0/24',
-    '176.126.206.0/23',
-    '185.7.190.0/23',
-    '185.18.250.0/24',
-    '185.38.135.0/24',
-    '185.59.218.0/24',
-    '185.67.124.0/24',
-    '185.72.49.0/24',
-    '185.109.21.0/24',
-    '185.135.9.0/24',
-    '185.148.104.0/22',
-    '185.162.228.0/22',
-    '185.170.166.0/24',
-    '185.174.138.0/24',
-    '185.176.24.0/24',
-    '185.176.26.0/24',
-    '185.193.28.0/22',
-    '185.201.139.0/24',
-    '185.213.240.0/24',
-    '185.213.243.0/24',
-    '185.221.160.0/24',
-    '185.234.22.0/24',
-    '185.238.228.0/24',
-    '185.244.106.0/24',
-    '188.42.88.0/23',
-    '188.244.122.0/24',
-    '191.101.251.0/24',
-    '192.65.217.0/24',
-    '192.133.11.0/24',
-    '193.9.49.0/24',
-    '193.16.63.0/24',
-    '193.17.206.0/24',
-    '193.67.144.0/24',
-    '193.188.14.0/24',
-    '193.227.99.0/24',
-    '194.1.194.0/24',
-    '194.36.49.0/24',
-    '194.36.55.0/24',
-    '194.36.216.0/22',
-    '194.40.240.0/23',
-    '194.53.53.0/24',
-    '194.152.44.0/24',
-    '194.169.194.0/24',
-    '195.85.23.0/24',
-    '195.85.59.0/24',
-    '195.137.167.0/24',
-    '195.245.221.0/24',
-    '196.13.241.0/24',
-    '196.207.45.0/24',
-    '199.60.103.0/24',
-    '199.181.197.0/24',
-    '199.212.90.0/24',
-    '202.82.250.0/24',
-    '203.13.32.0/24',
-    '203.17.126.0/24',
-    '203.19.222.0/24',
-    '203.22.223.0/24',
-    '203.23.103.0/24',
-    '203.23.104.0/24',
-    '203.23.106.0/24',
-    '203.24.102.0/23',
-    '203.24.108.0/23',
-    '203.28.8.0/23',
-    '203.29.52.0/22',
-    '203.30.188.0/22',
-    '203.32.120.0/23',
-    '203.34.28.0/24',
-    '203.34.80.0/24',
-    '203.55.107.0/24',
-    '203.89.5.0/24',
-    '203.107.173.0/24',
-    '203.193.21.0/24',
-    '204.62.141.0/24',
-    '204.68.111.0/24',
-    '204.209.72.0/23',
-    '205.233.181.0/24',
-    '206.196.23.0/24',
-    '207.189.149.0/24',
-    '208.100.60.0/24',
-    '212.24.127.0/24',
-    '212.110.134.0/23',
-    '212.239.86.0/24',
-    '216.120.180.0/23'
-  ],
-  Worldwide: [
-    '173.245.48.0/20',
-    '103.21.244.0/22',
-    '103.22.200.0/22',
-    '103.31.4.0/22',
-    '141.101.64.0/18',
-    '108.162.192.0/18',
-    '190.93.240.0/20',
-    '188.114.96.0/20',
-    '197.234.240.0/22',
-    '198.41.128.0/17',
-    '162.158.0.0/15',
-    '104.16.0.0/13',
-    '104.24.0.0/14',
-    '172.64.0.0/13',
-    '131.0.72.0/22'
-  ]
-};
+const findIP = (function () {
+  let runScan = false;
+  let validIPs = [];
 
-const urlParams = new URLSearchParams(window.location.search);
-
-let maxIP = urlParams.get('max')
-let testNo = 0;
-let validIPs = [];
-
-if (maxIP === null) {
-  maxIP = 20
-} else {
-  maxIP = ~~maxIP;
-}
-
-var ips = [];
-let numberOfWorkingIPs = 0;
-
-for (const cdnLocation in cfIPv4) {
-  for (const cidr of cfIPv4[cdnLocation]) {
-    ips = ips.concat(cidrToIpArray(cidr, cdnLocation));
-  }
-}
-testIPs(randomizeElements(ips));
-
-async function testIPs(ipList) {
-  const timeout = 2500;
-  for (const el of ipList) {
-    testNo++;
-    var testResult = 0;
-    const url = `https://${el.ip}/__down`;
-    const startTime = performance.now();
-    const controller = new AbortController();
-
-    for (const ch of ['', '|', '/', '-', '\\']) {
-      const timeoutId = setTimeout(() => {
-        controller.abort();
-      }, timeout);
-      if (ch) {
-        document.getElementById('test-no').innerText = `Test #${testNo}:`;
-        document.getElementById('ip-no').innerText = el.ip;
-        document.getElementById('ip-no').style = `color: green`;
-        document.getElementById('ip-try').innerText = ch;
-      } else {
-        document.getElementById('test-no').innerText = `Test #${testNo}:`;
-        document.getElementById('ip-no').innerText = el.ip;
-        document.getElementById('ip-no').style = `color: red`;
-        document.getElementById('ip-try').innerText = ch;
-      }
-      try {
-        const response = await fetch(url, {
-          signal: controller.signal,
-        });
-
-        testResult++;
-      } catch (error) {
-        if (error.name === "AbortError") {
-          //
-        } else {
-          testResult++;
+  function cidrToIpArray() {
+    let AllIPs = [];
+    for (const cdnLocation in cfIPv4) {
+      for (const cidr of cfIPv4[cdnLocation]) {
+        const parts = cidr.split('/');
+        const ip = parts[0];
+        const mask = parseInt(parts[1], 10);
+        const ipParts = ip.split('.');
+        const start = (
+          (parseInt(ipParts[0], 10) << 24) |
+          (parseInt(ipParts[1], 10) << 16) |
+          (parseInt(ipParts[2], 10) << 8) |
+          parseInt(ipParts[3], 10)
+        ) >>> 0; // convert to unsigned int
+        const end = (start | (0xffffffff >>> mask)) >>> 0; // convert to unsigned int
+      
+        const ips = [];
+        for (let i = start; i <= end; i++) {
+          const a = (i >> 24) & 0xff;
+          const b = (i >> 16) & 0xff;
+          const c = (i >> 8) & 0xff;
+          const d = i & 0xff;
+          ips.push({ip: `${a}.${b}.${c}.${d}`, location: cdnLocation});
         }
+        AllIPs = AllIPs.concat(ips);
       }
-      clearTimeout(timeoutId);
+    }  
+    return AllIPs;
+  }
+
+  function randomizeElements(IPs, max) {
+    const randIPs = [...IPs].sort(ip => {
+      return ip.location === 'London' ? 0.75 - Math.random() : 0.5 - Math.random();
+    });
+    return randIPs.slice(0, max);
+  }
+
+  async function testIPs(ipList, timeout, maxResults) {
+    let testNo = 0;
+    let numberOfWorkingIPs = 0;
+    validIPs = [];
+
+    for (const el of ipList) {
+      if(!runScan) break;
+      testNo++;
+      var testResult = 0;
+      const url = `https://${el.ip}/__down`;
+      const startTime = new Date().getTime();
+      const controller = new AbortController();
+  
+      for (const ch of ['', '|', '/', '-', '\\']) {
+        const timeoutId = setTimeout(() => {
+          controller.abort();
+        }, timeout);
+        document.querySelector('.status').innerHTML = `Test #${testNo}: ${el.ip} ${ch}`;
+        if (ch) {
+          document.querySelector('.status').style.color = 'green';
+        } else {
+          document.querySelector('.status').style.color = 'red';
+        }
+
+        try {
+          const response = await fetch(url, {
+            signal: controller.signal,
+          });
+          testResult++;
+        } catch (error) {
+          if (error.name === "AbortError") {
+            //
+          } else {
+            testResult++;
+          }
+        }
+        clearTimeout(timeoutId);
+      }
+  
+      const duration = (new Date().getTime()) - startTime;
+      if (testResult === 5) { 
+        numberOfWorkingIPs++;
+        validIPs.push({ip: el.ip, location: el.location, time: Math.floor(duration / 5)});
+
+        let progress = numberOfWorkingIPs * 100 / maxResults;
+        document.querySelector('.progress-bar').style.width = `${progress}%`;
+        document.querySelector('.progress-bar').innerHTML = `${progress}%`;
+
+        listResults();
+      }
+      if (numberOfWorkingIPs >= maxResults) {
+        break;
+      }
     }
+    document.querySelector('.status').innerHTML = `Scan Complete &#x2022 Scanned ${testNo}
+    IPs &#x2022 Found ${numberOfWorkingIPs} working IPs`;
+    document.querySelector('.start-scan-btn').innerHTML = "Start";
+  }
 
-    const duration = performance.now() - startTime;
+  function listResults() {
+    const sortedArr = validIPs.sort((a, b) => a.time - b.time);
+    const tableArr = sortedArr.map(obj => {
+      return `<tr><td></td><td>${obj.ip}</td><td>${obj.location}</td><td>${obj.time} ms</td>
+      <td><button class="copy-btn" title="Copy to clipboard"></button></td></tr>`;
+    });
 
-    if (testResult === 5) {
-      numberOfWorkingIPs++;
-      validIPs.push({ip: el.ip, location: el.location, time: Math.floor(duration / 5)});
-      const sortedArr = validIPs.sort((a, b) => a.time - b.time);
-      const tableRows = sortedArr.map(obj => `<tr><td></td><td>${obj.ip}</td><td>${obj.time}ms</td><td><img height="20px" src="assets/${obj.location}.png" /></td></tr>`).join('\n');
-      document.getElementById('result').innerHTML = tableRows;
-    }
+    const tableRows = tableArr.join('\n');
+    document.getElementById('best-table').innerHTML = tableArr[0];
+    document.getElementById('all-table').innerHTML = tableRows;
 
-    if (maxIP > 0 && numberOfWorkingIPs >= maxIP) {
-      break;
+    document.querySelectorAll('.copy-btn').forEach(btn => {
+      let ip = btn.parentNode.parentNode.childNodes[1].innerHTML;
+      btn.addEventListener('click', () => copyToClipboard(ip));
+    });
+    document.querySelector('.download-btn').addEventListener('click', downloadAsCSV);
+  }
+
+  function copyToClipboard(ip) {
+    navigator.clipboard.writeText(ip);
+  }
+
+  function downloadAsCSV() {
+    const csvString = validIPs.map(el => `${el.ip},${el.location},${el.time}`).join('\n');
+    const blob = new Blob([csvString], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'ip-list.csv');
+    link.style.display = 'none';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  return function(start) {
+    const timeout = Number(document.getElementById('timeout-list').value);
+    const maxScan = Number(document.getElementById('scan-ip-list').value);
+    const maxResults = Number(document.getElementById('results-list').value);
+    let scanArr = [];
+    if(start) {
+      runScan = true;
+      scanArr = randomizeElements(cidrToIpArray(), maxScan);
+      testIPs(scanArr, timeout, maxResults);
+    } else {
+      runScan = false;
+      return undefined;
     }
   }
-  document.getElementById('test-no').innerText = `DONE`;
-  document.getElementById('test-no').style = `color: green; font-weight: bold;`;
-  document.getElementById('ip-no').innerText = '';
-  document.getElementById('ip-try').innerText = '';
-}
+})();
 
-
-function cidrToIpArray(cidr, cdnLocation) {
-  const parts = cidr.split('/');
-  const ip = parts[0];
-  const mask = parseInt(parts[1], 10);
-  const ipParts = ip.split('.');
-  const start = (
-    (parseInt(ipParts[0], 10) << 24) |
-    (parseInt(ipParts[1], 10) << 16) |
-    (parseInt(ipParts[2], 10) << 8) |
-    parseInt(ipParts[3], 10)
-  ) >>> 0; // convert to unsigned int
-  const end = (start | (0xffffffff >>> mask)) >>> 0; // convert to unsigned int
-
-  const ips = [];
-  for (let i = start; i <= end; i++) {
-    const a = (i >> 24) & 0xff;
-    const b = (i >> 16) & 0xff;
-    const c = (i >> 8) & 0xff;
-    const d = i & 0xff;
-    ips.push({ip: `${a}.${b}.${c}.${d}`, location: cdnLocation});
+document.querySelector('.start-scan-btn').addEventListener('click', function () {
+  if(this.innerHTML === "Stop") {
+    this.innerHTML = "Start";
+    findIP(false);
+  } else {
+    this.innerHTML = "Stop";
+    document.querySelector('.progress-bar').style.width = '0%';
+    document.querySelector('.progress-bar').innerHTML = '0%';
+    findIP(true);
   }
-  return ips;
-}
-
-function randomizeElements(arr) {
-  return [...arr].sort((el1, el2) => {return el2.location === 'London' ? 0.75 - Math.random() : 0.5 - Math.random()});
-}
-
-function downloadAsCSV() {
-  const csvString = validIPs.map(el => el.ip).join('\n');
-  const blob = new Blob([csvString], { type: 'text/csv;charset=utf-8;' });
-  const url = URL.createObjectURL(blob);
-  const link = document.createElement('a');
-  link.setAttribute('href', url);
-  link.setAttribute('download', 'ip-list.csv');
-  link.style.display = 'none';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
+});
